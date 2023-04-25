16a481855fd57fbad8610d652d645b67e30a0fa6
Martin <velazquezochoaisrael@gmail.com>
2023-04-25 13:13:25 -0500

    solucion de router

--- Router.php
+++ Router.php
@@ -28,22 +28,18 @@ class Router
 
         // $auth = $_SESSION['login'] ?? null;
 
-        $currentUrl = $_SERVER['REQUEST_URI']==="" ?? '/' :: $_SERVER['REQUEST_URI'];
-        $method = $_SERVER['REQUEST_METHOD'];
-
-        if ($method === 'GET') {
-            $fn = $this->getRoutes[$currentUrl] ?? null;
-        } else {
-            $fn = $this->postRoutes[$currentUrl] ?? null;
-        }
-
-
-        if ( $fn ) {
-            // Call user fn va a llamar una función cuando no sabemos cual sera
-            call_user_func($fn, $this); // This es para pasar argumentos
-        } else {
-            echo "Página No Encontrada o Ruta no válida";
-        }
+        $currentUrl = ($_SERVER['REQUEST_URI'] === '') ? '/' :  $_SERVER['REQUEST_URI'] ;
+$method = $_SERVER['REQUEST_METHOD'];
+    
+//dividimos la URL actual cada vez que exista un '?' eso indica que se están pasando variables por la url
+$splitURL = explode('?', $currentUrl);
+// debuguear($splitURL);
+ 
+if ($method === 'GET') {
+    $fn = $this->getRoutes[$splitURL[0]] ?? null; //$splitURL[0] contiene la URL sin variables 
+} else {
+  $fn = $this->postRoutes[$splitURL[0]] ?? null;
+}
     }
 
     public function render($view, $datos = [])
